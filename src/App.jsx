311cf11ff09f98b15678db70be3fac91e72eb4b8
Aaryan120 <aaryanr356@gmail.com>
2026-02-27 12:32:10 +0530

    new testing

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from './components/Projects'
 // import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-
+// NEw comment added for testing
 const App = () => {
   return (
     <>
